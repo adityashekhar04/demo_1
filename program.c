4c3f8a44098829c76c09f85f57ac14a6eb76be29
Aditya Shekhar <adityashekhar04@gmail.com>
2023-05-04 22:46:32 +0530

    initial commitment

--- program.c
+++ program.c
@@ -4,5 +4,6 @@ int main(){
 cout<<"This is my first program"<<endl;
 cout<<"I have changed"<<endl;
 cout<<"I have chaned again"<<end;
+cout<<"I changed"<<endl;
 return 0;
 }
---
+++ program1.c
@@ -0,0 +1,5 @@
+#inculde<iosteam.h>
+using namespace std;
+int main(){
+cout<<"This is my program1";
+}
